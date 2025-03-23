2fcb176d31984aba0061064f885ee62ebb2dc598
damianlebiedz <damian.lebiedz@gmail.com>
2025-03-23 13:52:10 +0100

    commit

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-from modules.data_loader import load_closed_positions, load_open_positions, fetch_exchange_rates, fetch_market_data
-from modules.portfolio_calculator import calculate_daily_performance, calculate_portfolio
-from modules.plot_generator import plot_daily_performance, plot_portfolio
+from modules.data_loader import load_closed_positions, load_open_positions, fetch_exchange_rates
+from modules.portfolio_calculator import calculate_daily_portfolio_profit
+#from modules.plot_generator import plot_daily_performance, plot_portfolio
 
 if __name__ == "__main__":
     closed_positions = load_closed_positions("data/CLOSED POSITIONS.csv")
 
     exchange_rates = fetch_exchange_rates()
     symbols = closed_positions["Symbol"].unique().tolist()
-    market_data = fetch_market_data(symbols)
 
-    open_positions = load_open_positions("data/OPEN POSITIONS.csv", market_data, exchange_rates)
+    open_positions = load_open_positions("data/OPEN POSITIONS.csv", exchange_rates)
 
-    grouped_data = calculate_portfolio(open_positions)
-    plot_portfolio(grouped_data)
-
-    daily_performance = calculate_daily_performance(closed_positions, open_positions)
-    plot_daily_performance(daily_performance)
+    #BŁĄD - do zrobienia
+    portfolio_profit_df = calculate_daily_portfolio_profit(open_positions, closed_positions)
+    print(portfolio_profit_df)
--- modules/data_loader.py
+++ modules/data_loader.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import yfinance as yf
-from datetime import datetime, timedelta
+from datetime import datetime
 
 def load_closed_positions(file_path):
     try:
@@ -15,79 +15,12 @@ def load_closed_positions(file_path):
         print(f"Error in load_closed_positions: {e}")
         return pd.DataFrame()
 
-def load_open_positions(file_path, market_data, exchange_rates):
-    try:
-        df = pd.read_csv(file_path, sep=';')
-        print("Nazwy kolumn w df:", df.columns)
-
-        df['Formatted Symbol'] = df['Symbol'].apply(format_symbol_for_yf)
-        print("Sformatowane symbole:", df['Formatted Symbol'].unique())
-
-        for symbol in df['Formatted Symbol'].unique():
-            print(f"Processing symbol: {symbol}")
-            if symbol in market_data.index:
-                print(f"  Match found in market_data")
-                ticker = yf.Ticker(symbol)
-                close_price = ticker.fast_info.previous_close
-                df.loc[df['Formatted Symbol'] == symbol, 'Close price'] = close_price
-            else:
-                print(f"  No match found in market_data")
-                df.loc[df['Formatted Symbol'] == symbol, 'Close price'] = df.loc[df['Formatted Symbol'] == symbol, 'Open price']
-
-        df['Currency'] = df['Symbol'].apply(lambda x: x.split('.')[-1] if '.' in x else 'PLN')
-        print("Waluty:", df['Currency'].unique())
-
-        df['Exchange ratio'] = df['Currency'].apply(map_exchange_rate, args=(exchange_rates,))
-
-        for col in ['Open price', 'Close price', 'Purchase value', 'Volume', 'Exchange ratio']:
-            df[col] = df[col].astype(str).str.replace(',', '.').astype(float)
-
-        df['Sale value'] = df['Close price'] * df['Volume'] * df['Exchange ratio']
-        df['Profit'] = df['Sale value'] - df['Purchase value']
-
-        print("Open positions loaded successfully")
-        return df
-
-    except Exception as e:
-        print(f"Error in load_open_positions: {e}")
-        return pd.DataFrame()
-
-def fetch_exchange_rates():
-    try:
-        currencies = ["USD", "EUR", "GBP", "CHF", "JPY", "CAD", "AUD"]
-        tickers = [f"{cur}PLN=X" for cur in currencies]
-        rates = yf.download(tickers, period="1d")
-
-        exchange_rates = {cur: rates[("Close", f"{cur}PLN=X")].iloc[-1] for cur in currencies}
-        exchange_rates["PLN"] = 1.0
-        print("Exchange rates fetched successfully:", exchange_rates)
-        return exchange_rates
-
-    except Exception as e:
-        print(f"Error in fetch_exchange_rates: {e}")
-        return {}
-
 def format_symbol_for_yf(symbol):
     if symbol.endswith('.UK'):
         return symbol[:-3] + '.L'
     elif symbol.endswith('.US'):
         return symbol[:-3]
     return symbol
-
-def fetch_market_data(symbols, days_back=5):
-    try:
-        formatted_symbols = [format_symbol_for_yf(symbol) for symbol in symbols]
-        end_date = datetime.now()
-        start_date = end_date - timedelta(days=days_back)
-
-        market_data = yf.download(formatted_symbols, start=start_date, end=end_date)["Close"]
-        last_available_close = market_data.iloc[-1]
-        print("Market data fetched successfully")
-        return last_available_close
-
-    except Exception as e:
-        print(f"Error in fetch_market_data: {e}")
-        return pd.Series()
 
 def map_exchange_rate(currency, exchange_rates):
     if currency == 'US':
@@ -96,3 +29,48 @@ def map_exchange_rate(currency, exchange_rates):
         return exchange_rates.get('GBP', None)
     else:
         return exchange_rates.get(currency, None)
+
+def fetch_exchange_rates():
+    try:
+        currencies = ["USD", "EUR", "GBP"]
+        exchange_rates = {}
+
+        for cur in currencies:
+            ticker = f"{cur}PLN=X"
+            data = yf.download(ticker, period="1d")
+            exchange_rates[cur] = data["Close"].iloc[-1].item() if not data.empty else None
+
+        exchange_rates["PLN"] = 1.0
+        return exchange_rates
+
+    except Exception as e:
+        print(f"Error in fetch_exchange_rates: {e}")
+        return {}
+
+def load_open_positions(file_path, exchange_rates):
+    try:
+        df = pd.read_csv(file_path, sep=';')
+        df['Formatted Symbol'] = df['Symbol'].apply(format_symbol_for_yf)
+
+        for symbol in df['Formatted Symbol'].unique():
+            ticker = yf.Ticker(symbol)
+            close_price = ticker.fast_info.regular_market_previous_close
+            df.loc[df['Formatted Symbol'] == symbol, 'Close price'] = close_price
+
+        df['Currency'] = df['Symbol'].apply(lambda x: x.split('.')[-1] if '.' in x else 'PLN')
+        df['Exchange ratio'] = df['Currency'].apply(map_exchange_rate, args=(exchange_rates,))
+
+        for col in ['Open price', 'Close price', 'Purchase value', 'Volume', 'Exchange ratio']:
+            df[col] = df[col].astype(str).str.replace(',', '.').astype(float)
+
+        df['Current value'] = df['Close price'] * df['Volume'] * df['Exchange ratio']
+        df['Profit'] = df['Current value'] - df['Purchase value']
+
+        df['Close time'] = datetime.now().replace(microsecond=0)
+
+        print("Open positions loaded successfully")
+        return df
+
+    except Exception as e:
+        print(f"Error in load_open_positions: {e}")
+        return pd.DataFrame()
