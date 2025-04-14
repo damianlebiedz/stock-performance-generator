6ee9b18779e87d2945b9282252e6185ba98140e2
damianlebiedz <damian.lebiedz@gmail.com>
2025-04-14 13:22:49 +0200

    commit

--- modules/summary_loader.py
+++ modules/summary_loader.py
@@ -10,7 +10,7 @@ def load_summary_timeframe(all_stocks):
             info['Timeframe']['Date'].max() for stock in all_stocks for info in stock['Informations']
         )
         business_days = pd.date_range(start=start_date.date(), end=end_date.date())
-        timeframe = pd.DataFrame({'Date': business_days, 'Profit': 0.0, 'Profit cumulated': 0.0, 'Daily change': 0.0,
+        timeframe = pd.DataFrame({'Date': business_days, 'Total profit': 0.0,
                                   'Total change': 0.0})  # dodać in PLN (ryzyko walutowe) każdego profitu
 
         return timeframe
@@ -18,6 +18,7 @@ def load_summary_timeframe(all_stocks):
     except Exception as e:
         print(f"Error in summary_timeframe: {e}")
         return pd.DataFrame()
+
 
 def load_summary(all_stocks):
     summary_timeframe = load_summary_timeframe(all_stocks)
@@ -28,11 +29,11 @@ def load_summary(all_stocks):
 
                 for index, row in timeframe.iterrows():
                     date = row['Date']
-                    profit = row['Profit']
+                    profit = row['Total profit']
 
-                    summary_timeframe.loc[summary_timeframe['Date'] == date, 'Profit'] += profit
+                    summary_timeframe.loc[summary_timeframe['Date'] == date, 'Total profit'] += profit
 
-        filtered_summary_timeframe = summary_timeframe[summary_timeframe['Profit'] != 0.0]
+        filtered_summary_timeframe = summary_timeframe[summary_timeframe['Total profit'] != 0.0]
 
         return filtered_summary_timeframe
 
