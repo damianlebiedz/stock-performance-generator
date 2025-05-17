54081c51444ce1b0fbe49f665f43b0c3475c0f46
damianlebiedz <damian.lebiedz@gmail.com>
2025-05-17 11:21:30 +0200

    Add comparison_ticker to the controller

--- modules/controller.py
+++ modules/controller.py
@@ -1,5 +1,12 @@
 # CONTROLLER
 
+# If you are using a history of transactions from XStation5 platform from XTB broker, set True
+# Otherwise you should format your history of transactions as indicated in data/README.md file, then set False
+XStation5 = False
+
+# TO DO
+comparison_ticker = "^SPX"
+
 def format_symbol_for_yf(symbol):
     if symbol.endswith('.UK'):
         return symbol[:-3] + '.L'
